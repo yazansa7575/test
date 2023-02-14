2e7ed8a00fb0a9ac3ca500856ded17e48750e08a
Yazan Sawan <107117438+yazansa7575@users.noreply.github.com>
2023-02-14 22:03:26 +0200

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import './app.css'
+function App() {
+  return (
+    <>
+      <header>
+        <h1 className="header">header</h1>
+      </header>
+      <footer>
+        <h1>footer</h1>
+      </footer>
+    </>
+  );
+}
+
+export default App;
